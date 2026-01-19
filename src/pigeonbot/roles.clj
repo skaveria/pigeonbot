37efeb58d9b3b1edd4533d2b7f1a3a4721dcd75d
Nichole Kernreicht <nicholekernreicht@Nicholes-iMac.local>
2026-01-19 15:11:48 -0600

    updated roles

--- src/pigeonbot/roles.clj
+++ src/pigeonbot/roles.clj
@@ -1,18 +1,20 @@
 (ns pigeonbot.roles
-  (:require [clojure.set :as set]
-            [discljord.messaging :as m]
+  (:require [discljord.messaging :as m]
             [pigeonbot.config :as config]
             [pigeonbot.state :refer [state]]))
 
 (defn allowed-role-ids
-  "Return a set of role IDs (longs) that are self-assignable."
   []
   (->> (get (config/load-config) :self-role-ids [])
        (map long)
        set))
 
+(defn list-roles
+  "Return allowed self-assignable role IDs."
+  []
+  (sort (allowed-role-ids)))
+
 (defn add-role!
-  "Add a role to a user by raw role-id."
   [guild-id user-id role-id]
   (let [role-id (long role-id)]
     (cond
@@ -32,7 +34,6 @@
         {:ok? true :role-id role-id}))))
 
 (defn remove-role!
-  "Remove a role from a user by raw role-id."
   [guild-id user-id role-id]
   (let [role-id (long role-id)]
     (cond
@@ -50,8 +51,3 @@
          (long user-id)
          role-id)
         {:ok? true :role-id role-id}))))
-
-(defn list-roles
-  "Return allowed role IDs."
-  []
-  (sort (allowed-role-ids)))
