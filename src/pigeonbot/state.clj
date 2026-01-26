a304a0f6bf356c79a0711f395a6bc4827089efb0
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-01-26 13:27:59 -0600

    updated state

--- src/pigeonbot/state.clj
+++ src/pigeonbot/state.clj
@@ -1,6 +1,5 @@
 (ns pigeonbot.state)
-
+;; Runtime state for the bot process.
+;; Keys (when running): :connection :events :messaging
 (defonce state
-  "Runtime state for the bot process.
-   Keys (when running): :connection :events :messaging"
   (atom nil))
