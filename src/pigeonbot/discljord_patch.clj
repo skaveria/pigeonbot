3ee7e8897f9e3d9671ebebc5821ae025d8fa9ef8
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-02-06 12:13:06 -0600

    updated patch

--- src/pigeonbot/discljord_patch.clj
+++ src/pigeonbot/discljord_patch.clj
@@ -1,11 +1,24 @@
 (ns pigeonbot.discljord-patch
   (:require [clojure.data.json :as json]
-            [clojure.string :as str]
-            [discljord.messaging.impl :as impl]))
+            [clojure.string :as str]))
 
-(defn patch-discljord-json-body! []
-  (alter-var-root #'impl/json_body
-    (fn [_]
-      (fn [body]
-        (when (and body (not (str/blank? body)))
-          (json/read-str body :key-fn keyword))))))
+(defn patch-discljord-json-bodies!
+  "Patch discljord JSON response parsing to tolerate nil/blank bodies.
+  Patches both json_body and json-body if present (vars may be private)."
+  []
+  (require 'discljord.messaging.impl)
+  (let [interns (ns-interns 'discljord.messaging.impl)
+        targets (->> ['json_body 'json-body]
+                     (map interns)
+                     (remove nil?))]
+    (when (empty? targets)
+      (throw (ex-info "No json body vars found in discljord.messaging.impl"
+                      {:available (->> (keys interns) sort vec)})))
+    (doseq [v targets]
+      (alter-var-root v
+        (fn [_orig]
+          (fn [body]
+            (when (and body (not (str/blank? body)))
+              (json/read-str body :key-fn keyword))))))
+    {:patched (->> targets (map (comp :name meta)) (map str) vec)
+     :count (count targets)}))
