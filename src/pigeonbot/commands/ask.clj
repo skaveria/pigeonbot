a11610546694f5797b04c4b2973804f134f2ff0a
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-02-06 20:30:09 -0600

    updated ask

--- src/pigeonbot/commands/ask.clj
+++ src/pigeonbot/commands/ask.clj
@@ -14,8 +14,9 @@
 
 (defn- message-starts-with-pigeonbot-mention? [content]
   (when-let [bid (bot-id)]
-    (boolean (re-find (re-pattern (str "^\\s*<@!?" (java.util.regex.Pattern/quote bid) ">\\s*"))
-                      (or content "")))))
+    (boolean
+     (re-find (re-pattern (str "^\\s*<@!?" (java.util.regex.Pattern/quote bid) ">\\s*"))
+              (or content "")))))
 
 (defn- strip-leading-pigeonbot-mention [s]
   (if-let [bid (bot-id)]
@@ -24,12 +25,37 @@
         str/trim)
     (str/trim (or s ""))))
 
-(defn- reply-to-bot? [msg]
+(defn- referenced-message-reply-to-bot?
+  "Some payloads include full :referenced_message."
+  [msg]
   (true? (get-in msg [:referenced_message :author :bot])))
 
-(defn- mentioned-pigeonbot? [{:keys [mentions]}]
+(defn- message-reference-id
+  "Some payloads only include :message_reference {:message_id ...} without :referenced_message."
+  [msg]
+  (or (get-in msg [:message_reference :message_id])
+      (get-in msg [:message_reference :message-id])
+      (get-in msg [:message-reference :message_id])
+      (get-in msg [:message-reference :message-id])))
+
+(defn- reply-to-bot?
+  "Robust reply detection:
+  - If :referenced_message exists and is a bot -> true
+  - Else, if :message_reference points at a message id that we recently recorded as bot -> true"
+  [{:keys [channel-id] :as msg}]
+  (or (referenced-message-reply-to-bot? msg)
+      (when-let [rid (some-> (message-reference-id msg) str)]
+        (some (fn [{:keys [id bot?]}]
+                (and (= id rid) bot?))
+              (ctx/recent-messages channel-id)))))
+
+(defn- mentioned-pigeonbot?
+  "True if this message mentions pigeonbot specifically (by id)."
+  [{:keys [mentions]}]
   (when-let [bid (bot-id)]
-    (boolean (some (fn [m] (= (str (:id m)) bid)) (or mentions [])))))
+    (boolean
+     (some (fn [m] (= (str (:id m)) bid))
+           (or mentions [])))))
 
 (defn- build-ask-context [msg]
   (ctx/context-text msg))
@@ -42,9 +68,11 @@
      (if (str/blank? question)
        (u/send! channel-id :content "Usage: !ask <question> (or reply / @mention me)")
        (do
+         ;; quick ack
          (if reply-to-id
            (u/send-reply! channel-id reply-to-id :content "Hm. Lemme think…")
            (u/send! channel-id :content "Hm. Lemme think…"))
+
          (future
            (try
              (let [context-text (build-ask-context msg)
@@ -63,9 +91,11 @@
   "If reply to pigeonbot OR message begins with pigeonbot mention, run ask pipeline."
   [{:keys [content id] :as msg}]
   (cond
+    ;; replies: always reply back (keeps threads tidy)
     (reply-to-bot? msg)
     (do (run-ask! msg content id) true)
 
+    ;; mention: only if pigeonbot is the prefix
     (and (or (message-starts-with-pigeonbot-mention? content)
              (mentioned-pigeonbot? msg))
          (message-starts-with-pigeonbot-mention? content))
