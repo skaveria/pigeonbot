df4da3d25158aaa48c08910ba4edffac1c66b4d1
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-02-07 13:14:38 -0600

    added clawbot bridge

---
+++ src/pigeonbot/brain.clj
@@ -0,0 +1,16 @@
+(ns pigeonbot.brain
+  (:require [pigeonbot.config :as config]
+            [pigeonbot.ollama :as ollama]
+            [pigeonbot.brains.openclaw :as openclaw]))
+
+(defn brain-type []
+  (let [cfg (config/load-config)]
+    (or (:brain cfg) :ollama)))
+
+(defn ask-with-context
+  "Unified brain entry point."
+  [context-text question]
+  (case (brain-type)
+    :openclaw (openclaw/ask-with-context context-text question)
+    ;; default
+    :ollama   (ollama/geof-ask-with-context context-text question)))
