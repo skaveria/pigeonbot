736da768368f89b7c9d7699b1c3978b5dc96f117
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-02-06 20:27:32 -0600

    updated dispatch

--- src/pigeonbot/commands/dispatch.clj
+++ src/pigeonbot/commands/dispatch.clj
@@ -1,9 +1,11 @@
 (ns pigeonbot.commands.dispatch
-  (:require [clojure.string :as str]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [pigeonbot.custom-commands :as custom]
             [pigeonbot.commands.registry :as reg]
             [pigeonbot.commands.util :as u]
             [pigeonbot.commands.ask :as ask]
+            ;; load modules for side-effect registration via defcmd
             [pigeonbot.commands.builtins]
             [pigeonbot.commands.roles]
             [pigeonbot.commands.custom]
@@ -19,18 +21,29 @@
   [{:keys [content channel-id] :as msg}]
   (let [cmd (first (str/split (or content "") #"\s+"))]
     (cond
+      ;; 1) built-ins
       (when-let [cmd-fn (@reg/commands cmd)]
         (cmd-fn msg)
         true)
       nil
 
+      ;; 2) custom commands
       (when-let [r (custom/registered-reply cmd)]
         (case (:type r)
-          :url  (do (u/send! channel-id :content (:url r)) true)
-          :file (do (u/send-file! channel-id (io/file "src/pigeonbot/media" (:file r))) true)
+          :url
+          (do (u/send! channel-id :content (:url r)) true)
+
+          ;; back-compat if you still have old file-based custom commands
+          :file
+          (let [f (io/file "src/pigeonbot/media" (:file r))]
+            (when (.exists f)
+              (u/send-file! channel-id f))
+            true)
+
           nil))
       nil
 
+      ;; 3) ask-like triggers
       (ask/handle-ask-like! msg)
       true
 
