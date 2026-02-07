182adf2c9e38d1826c01f252b5bded10805bb633
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-02-07 16:25:29 -0600

    updated vision reacts

--- src/pigeonbot/vision_reacts.clj
+++ src/pigeonbot/vision_reacts.clj
@@ -7,7 +7,16 @@
 
 (defonce ^:private reacted-message-ids* (atom #{}))
 
-(def ^:private default-emoji ":tposs:")
+(def ^:private default-emoji "🦝") ;; temp default for debugging
+
+(defn- cfg []
+  (let [m (config/load-config)]
+    {:debug? (true? (:vision-debug? m))
+     :emoji  (or (:tposs-emoji m) default-emoji)}))
+
+(defn- log! [& xs]
+  (when (:debug? (cfg))
+    (apply println xs)))
 
 (defn- bot-message? [msg]
   (true? (get-in msg [:author :bot])))
@@ -17,9 +26,9 @@
        (str/starts-with? (str/triml content) "!")))
 
 (defn- image-attachment?
-  "Heuristic: treat as image if content-type starts with image/ OR filename has image extension."
-  [{:keys [content-type content_type filename url]}]
-  (let [ct (or content-type content_type "")
+  "Heuristic: treat as image if content-type starts with image/ OR filename/url has image extension."
+  [{:keys [content-type content_type contentType filename url]}]
+  (let [ct (or content-type content_type contentType "")
         fn (or filename "")
         u  (or url "")]
     (or (and (string? ct) (str/starts-with? (str/lower-case ct) "image/"))
@@ -27,17 +36,25 @@
         (re-find #"\.(png|jpe?g|gif|webp)$" (str/lower-case u)))))
 
 (defn- first-image-url [msg]
-  (some->> (or (:attachments msg) [])
-           (filter image-attachment?)
-           first
-           :url))
+  (let [atts (or (:attachments msg) [])]
+    (log! "vision-reacts: attachments count =" (count atts))
+    (when (seq atts)
+      (log! "vision-reacts: first attachment keys =" (-> atts first keys sort vec))
+      (log! "vision-reacts: first attachment sample =" (pr-str (select-keys (first atts)
+                                                                            [:id :filename :url :proxy_url :size
+                                                                             :content_type :content-type :contentType]))))
+    (some->> atts
+             (filter image-attachment?)
+             first
+             :url)))
 
 (defn- normalize-emoji
   "Accepts:
-   - \":tposs:\" (name only)
+   - unicode emoji (\"🦝\")
+   - \":tposs:\" (name only; may or may not work for custom)
    - \"tposs:123\"
    - \"<:tposs:123>\"
-  Returns string suitable for discljord create-reaction!."
+  Returns a string to pass to discljord create-reaction!."
   [s]
   (let [s (str/trim (str s))]
     (cond
@@ -47,39 +64,58 @@
       (seq s) s
       :else default-emoji)))
 
-(defn- tposs-emoji []
-  (let [cfg (config/load-config)]
-    (normalize-emoji (or (:tposs-emoji cfg) default-emoji))))
+(defn- chosen-emoji []
+  (normalize-emoji (:emoji (cfg))))
 
 (defn- react!
   [channel-id message-id emoji]
   (when-let [messaging (:messaging @state)]
-    ;; discljord provides create-reaction! in modern versions; if not, you'll see a var error.
+    (log! "vision-reacts: reacting" {:channel-id (str channel-id)
+                                    :message-id (str message-id)
+                                    :emoji emoji})
     (m/create-reaction! messaging channel-id message-id emoji)))
 
 (defn maybe-react-opossum!
   "If msg has an image attachment and OpenClaw says it contains an opossum,
-  react with :tposs: (or configured emoji). Returns true if reacted."
+  react with configured emoji. Returns true if it kicked off processing."
   [{:keys [id channel-id content] :as msg}]
-  (when (and id channel-id
-             (not (bot-message? msg))
-             (not (command-message? content))
-             (not (contains? @reacted-message-ids* (str id))))
-    (when-let [img (first-image-url msg)]
-      (swap! reacted-message-ids* conj (str id))
-      (future
-        (try
-          (when (oc/opossum-in-image? img)
-            (try
-              (react! channel-id id (tposs-emoji))
-              (catch Throwable t
-                (println "vision-reacts: failed to add reaction:" (.getMessage t)
-                         {:channel-id (str channel-id)
-                          :message-id (str id)
-                          :emoji (tposs-emoji)}))))
-          (catch Throwable t
-            ;; allow future retries if OpenClaw was down; remove from reacted set
-            (swap! reacted-message-ids* disj (str id))
-            (println "vision-reacts: opossum check failed:" (.getMessage t)
-                     (or (ex-data t) {})))))
-      true)))
+  (let [mid (some-> id str)]
+    (cond
+      (nil? id)
+      (do (log! "vision-reacts: skip (no id)") nil)
+
+      (nil? channel-id)
+      (do (log! "vision-reacts: skip (no channel-id)") nil)
+
+      (bot-message? msg)
+      (do (log! "vision-reacts: skip (author is bot)") nil)
+
+      (command-message? content)
+      (do (log! "vision-reacts: skip (command message)" (pr-str content)) nil)
+
+      (contains? @reacted-message-ids* mid)
+      (do (log! "vision-reacts: skip (already processed)" mid) nil)
+
+      :else
+      (let [img (first-image-url msg)]
+        (if-not (seq (str img))
+          (do (log! "vision-reacts: no image url found; skip") nil)
+          (do
+            (swap! reacted-message-ids* conj mid)
+            (log! "vision-reacts: image url =" img)
+            (future
+              (try
+                (log! "vision-reacts: calling OpenClaw opossum-in-image? ...")
+                (let [yes? (oc/opossum-in-image? img)]
+                  (log! "vision-reacts: OpenClaw result =" yes?)
+                  (when yes?
+                    (let [emoji (chosen-emoji)
+                          ch (react! channel-id id emoji)
+                          resp (when (instance? clojure.lang.IDeref ch)
+                                 (deref ch 8000 :timeout))]
+                      (log! "vision-reacts: reaction response =" (pr-str resp)))))
+                (catch Throwable t
+                  ;; allow future retries if OpenClaw was down; remove from processed set
+                  (swap! reacted-message-ids* disj mid)
+                  (log! "vision-reacts: ERROR:" (.getMessage t) (or (ex-data t) {})))))
+            true))))))
